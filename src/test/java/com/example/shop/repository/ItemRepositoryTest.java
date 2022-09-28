ff94ba506a82cd0b16059b6d0c06a34709860a0e
komawer02 <komawer02@naver.com>
2022-09-28 14:50:16 +0900

    second commit

---
+++ src/test/java/com/example/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.example.shop.repository;
+
+import com.example.shop.constant.ItemSellStatus;
+import com.example.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest // 테스트 실행시 모든 Bean을 IoC 컨테이너에 등록 = 스프링부트 구동과 동일한 환경 조건
+@TestPropertySource(locations="classpath:application-test.properties") // 테스트 실행시 우선되는 설정파일
+class ItemRepositoryTest {
+    @Autowired // 필드 Bean 주입
+    ItemRepository itemRepository;
+
+    @Test // Method 테스트 대상 지정
+    @DisplayName("상품 저장 확인") // 테스트명
+    public void createItemTest(){
+        Item item = new Item();
+        item.setItemNm("좋은 상품");
+        item.setPrice(10000);
+        item.setItemDetail("좋은 상품 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+        Item savedItem = itemRepository.save(item);
+        System.out.println(savedItem.toString());
+    }
+}
